b23b23d5b8681117659bf247f6a3088c7239f01f
M. Sener OGUZ <mseneroguz@gmail.com>
2022-09-17 20:34:33 +0300

    ek

--- src/test/java/day07/C03_DropDown.java
+++ src/test/java/day07/C03_DropDown.java
@@ -6,15 +6,16 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
 
 import java.time.Duration;
 import java.util.List;
 
-public class C02_DropDownHandle {
+public class C03_DropDown { //TODO: Bu class C02_DropDownHandle class'inin Select Class'indan yardim alinarak cozumudur!!!!!!!!!!!!!
+
     //● Bir class oluşturun: C3_DropDownAmazon
     //● https://www.amazon.com/ adresine gidin.
     //- Test 1
@@ -34,46 +35,30 @@ public class C02_DropDownHandle {
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
         driver.get("https://www.amazon.com/");
+
     }
 
     @After
     public void tearDown(){
-        driver.close();
+        //driver.close();
     }
-
+    //- Test 1
     //Arama kutusunun yanindaki kategori menusundeki kategori sayisinin 45
     //oldugunu test edin
     @Test
     public void test1(){
-        List<WebElement> drops=driver.findElements((By.xpath("//option")));
-        System.out.println(drops.size());
-        for (WebElement each:drops
-             ) {
-            System.out.println(each.getText());
-        }
-        int dropDownList= drops.size();
+        WebElement ddm= driver.findElement(By.xpath("//*[@id='searchDropdownBox']"));
+        Select select=new Select(ddm);
+        /*
+        DropDown menuye ulasmak icin Select class'indan bir obje olustururuz ve locate ettigimiz dropdown webelement'ini
+        Select class'ina tanimlariz ve getOption methodu'nu kullanarak dropdown'u bir list'e atarak dropdown menunun
+        butun elemanlarinin sayisina ulasabiliriz.
+         */
+        List<WebElement> ddmList=select.getOptions();
+        System.out.println(ddmList.size());
         int expectedSayi=45;
-        Assert.assertNotEquals(expectedSayi,dropDownList);
-    }
-
-    //-Test 2
-    @Test
-    public void test2(){
-    WebElement ddm= driver.findElement(By.xpath("//*[@id='searchDropdownBox']"));
-        //1. Kategori menusunden Books secenegini secin
-    ddm.sendKeys(("Books"));
-
-        //2. Arama kutusuna Java yazin ve aratin
-    driver.findElement(By.cssSelector("input[id='twotabsearchtextbox']")).sendKeys("Java"+ Keys.ENTER);
-
-        //3. Bulunan sonuc sayisini yazdirin
-    WebElement souncYazisi= driver.findElement(By.xpath("//*[@class='a-section a-spacing-small a-spacing-top-small']"));
-        System.out.println(souncYazisi.getText());
-
-        //4. Sonucun Java kelimesini icerdigini test edin
-        String expectedKelime="Java";
-        String actualSonucYazisi=souncYazisi.getText();
-        Assert.assertTrue(actualSonucYazisi.contains(expectedKelime));
+        int actualDds= ddmList.size();
+        Assert.assertNotEquals(expectedSayi,actualDds);
     }
 
 }
