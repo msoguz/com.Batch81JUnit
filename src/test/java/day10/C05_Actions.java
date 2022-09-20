60fdd834ecf45a74d2b57314d7e0c78574af6ab0
M. Sener OGUZ <mseneroguz@gmail.com>
2022-09-20 23:21:11 +0300

    ek

--- src/test/java/day10/C05_Actions.java
+++ src/test/java/day10/C05_Actions.java
@@ -1,5 +1,6 @@
 package day10;
 
+import org.junit.Assert;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
@@ -19,12 +20,12 @@ public class C05_Actions extends TestBaseBeforeAfter {
         //2 “Drag me” butonunu tutup “Drop here” kutusunun ustune birakalim
         WebElement dragMe= driver.findElement(By.xpath("//*[@id='draggable']"));
         WebElement dropHere=driver.findElement(By.xpath("(//*[@id='droppable'])[1]"));
-        Actions actions=new Actions(driver);
+        Actions actions=new Actions(driver); //Actions objesini TestBase Class'ta da aolusturabilir ve burada kullanmayaniliriz.
         Thread.sleep(3000);
         actions.dragAndDrop(dragMe,dropHere).perform();
 
         //3 “Drop here” yazisi yerine “Dropped!” oldugunu test edin
-
+        Assert.assertEquals("Dropped!",driver.findElement(By.xpath("//*[text()='Dropped!']")).getText());
     }
 
 
